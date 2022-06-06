201cddab11e39484f9bb1a6fef79ff6dd7615f62
root <root@pratique.centos8>
2022-06-06 07:56:10 -0400

    d

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###Package d'installation
+#Author : Alain Eboeul
+#Date : 06-06-2022
+
+if ( $UID=0 )
+then
+echo "Sorry!!! You must be the admin to execute this script"
+else 
+	echo "hello "
+fi
