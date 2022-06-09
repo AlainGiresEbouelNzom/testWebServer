113c08993cb9571cf027b84eacaba8c6f35291cd
ALAIN GIRES EBOUEL NZOM <90434281+AlainGiresEbouelNzom@users.noreply.github.com>
2022-06-09 16:23:43 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,43 +1,72 @@
 #!/bin/bash
-###Package d'installation
-#Author : Alain Eboeul
-#Date : 06-06-2022
 
+echo -e " \n\n=========== PREPARATION.....========"
 
-echo "Enter 3 numbers"
-read nb1
-read nb2
-read nb3
-
-
-if [ $nb1 -gt $nb2 ]
+#Check if nmap exist, and intall it if not
+if [ ! -e /usr/bin/nmap ]
 then
-        if [ $nb2 -gt $nb3 ]
-                then echo "$nb1, $nb2, $nb3"
-        else
-                if  [ $nb3 -lt $nb1 ]
-                then
-                        echo "$nb1, $nb3, $nb2"
+        echo -e "\n -----nmap installation ----"
+        yum install -y nmap
 
-                else
-                         echo "$nb3, $nb1, $nb2"
-
-                fi
-
-        fi
-else
-        if [ $nb1 -gt $nb3 ]
-                then echo "$nb2, $nb1, $nb3"
-        else
-                if  [ $nb3 -lt $nb2 ]
-                then
-                        echo "$nb2, $nb3, $nb1"
-
-                else
-                         echo "$nb3, $nb2, $nb1"
-
-                fi
-
-        fi
 fi
+
+
+echo -e "\n**********INVENTORY CHECKING*******\n"
+echo "-----OS and Kernel----"
+touch tempFile
+hostnamectl | grep  Operating
+hostnamectl | grep  Kernel
+
+echo -e "\n----CPU and Memory----"
+grep "cpu MHz" -m 1 /proc/cpuinfo
+grep "model name" -m 1 /proc/cpuinfo
+grep MemTotal -m 1 /proc/meminfo
+
+#Os running and vers
+#sed -n '7'p tempFile
+
+#Kernel version
+#sed -n '9'p tempFile
+
+
+#Cpu speed
+#sed -n '1'p /proc/cpuinfo
+
+#memory speed
+#sed -n '1'p /proc/meminfo
+
+#Size of harddrive
+
+echo -e "\n----Hard Drive infos---"
+fdisk -l | grep Device
+fdisk -l | grep dev
+
+echo -e "\n ----System bits----"
+hostnamectl | grep Architecture -i
+
+echo -e "\n---hostname---"
+hostname
+
+echo -e "\n----ip adress-----"
+hostname -I
+
+echo -e "\n ---open ports----"
+nmap $HOSTNAME |grep PORT -i
+nmap $HOSTNAME |grep open -i
+
+
+echo -e "\n ---DNS----"
+grep nameserver -i /etc/resolv.conf
+
+
+echo -e "\n ----Virtual or Phisical ?----"
+hostnamectl | grep chassis -i
+
+echo -e "\n ----MAC Adress ?----"
+ifconfig | grep ether -i
+#cat tempFile
+
+echo " "
+echo "*********END*************"
+echo " "
 
